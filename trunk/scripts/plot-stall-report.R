1d7ef3cd87b0b8fea205c9c9d42f49b68179c4a2
camellyx@gmail.com <camellyx@gmail.com@eb8839f4-635f-18d2-e397-3938a0a04158>
2014-01-22 05:42:33 +0000

    r script

--- trunk/scripts/plot-stall-report.R
+++ trunk/scripts/plot-stall-report.R
@@ -4,8 +4,8 @@ args <- commandArgs(TRUE)
 
 table <- read.table(args[1])
 
-#heip <- hist(table$V4, breaks=12, col="black")
-#plot(deip)
+heip <- hist(table$V4, breaks=12, col="black")
+plot(deip)
 
 darr <- density(table$V1)
 plot(darr)
